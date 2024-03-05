8017f0aa0d075959f4e1ddfb003fd197f80a9194
pyncer <root@pyncer.com>
2024-03-04 23:51:55 -0500

    Added include-values user mapper query option.

--- src/Table/User/UserMapperQuery.php
+++ src/Table/User/UserMapperQuery.php
@@ -4,8 +4,37 @@ namespace Pyncer\Snyppet\Access\Table\User;
 use Pyncer\Data\MapperQuery\AbstractRequestMapperQuery;
 use Pyncer\Snyppet\Access\User\UserGroup;
 
+use function Pyncer\Array\unset_keys as pyncer_array_unset_keys;
+
 class UserMapperQuery extends AbstractRequestMapperQuery
 {
+    public function overrideModel(
+        ModelInterface $model,
+        array $data
+    ): ModelInterface
+    {
+        if (!$this->getOptions()) {
+            return $model
+        }
+
+        if ($this->getOptions()->hasOption('include-values')) {
+            $result = $this->getConnection()->select('user__value')
+                ->columns('key', 'value')
+                ->where(['user_id' => $model->getId()])
+                ->result();
+
+            $extraData = [];
+            foreach ($result as $row) {
+                $extraData[$row['key']] = $row['value'];
+            }
+
+            $extraData = pyncer_array_unset_keys($extraData, $model->getKeys());
+            $model->setExtraData($extraData);
+        }
+
+        return $model;
+    }
+
     protected function isValidFilter(
         string $left,
         mixed $right,
@@ -32,5 +61,15 @@ class UserMapperQuery extends AbstractRequestMapperQuery
         }
 
         return parent::isValidFilter($left, $right, $operator);
+    }
+
+    protected function isValidOption(string $option): bool
+    {
+        switch ($option) {
+            case 'include-values':
+                return true;
+        }
+
+        return parent::isValidOption($option);
     }
 }
